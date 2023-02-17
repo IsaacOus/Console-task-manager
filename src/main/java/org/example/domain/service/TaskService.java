40b843adf0ad4f8a6cb7c4703bb29bda8f2ff134
IsaacOus <iouslimane@myges.fr>
2023-02-17 19:37:54 +0100

    Simplified Task Builder

--- src/main/java/org/example/domain/service/TaskService.java
+++ src/main/java/org/example/domain/service/TaskService.java
@@ -1,4 +1,17 @@
 package org.example.domain.service;
 
+import org.example.domain.model.Task;
+import org.example.domain.repository.TaskRepository;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public interface TaskService {
+
+        Task addTask(String description, LocalDateTime creationDate, Optional<LocalDateTime> dueDate, Optional<LocalDateTime> closeDate, Optional<String> tag, List<Task> subTasks);
+        Task updateTask();
+        boolean removeTask();
+        Task getTask();
+        List<Task> getAllTasks();
 }
--- src/main/java/org/example/infrastructure/service/TaskServiceImpl.java
+++ src/main/java/org/example/infrastructure/service/TaskServiceImpl.java
@@ -1,6 +1,54 @@
 package org.example.infrastructure.service;
 
+import org.example.domain.model.Task;
+import org.example.domain.model.TaskBuilder;
+import org.example.domain.repository.TaskRepository;
 import org.example.domain.service.TaskService;
 
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
 public class TaskServiceImpl implements TaskService {
+
+    private final TaskRepository taskRepository;
+
+    public TaskServiceImpl(TaskRepository taskRepository) {
+        this.taskRepository = taskRepository;
+    }
+
+
+    @Override
+    public Task addTask(String description, LocalDateTime creationDate, Optional<LocalDateTime> dueDate, Optional<LocalDateTime> closeDate, Optional<String> tag, List<Task> subTasks) {
+        Task task = new TaskBuilder()
+                .setDescription(description)
+                .setCreationDate(creationDate)
+                .dueDate(dueDate)
+                .closeDate(closeDate)
+                .tag(tag)
+                .subTasks(subTasks)
+                .getResult();
+
+        return taskRepository.addTask(task);
+    }
+
+    @Override
+    public Task updateTask() {
+        return null;
+    }
+
+    @Override
+    public boolean removeTask() {
+        return false;
+    }
+
+    @Override
+    public Task getTask() {
+        return null;
+    }
+
+    @Override
+    public List<Task> getAllTasks() {
+        return null;
+    }
 }
