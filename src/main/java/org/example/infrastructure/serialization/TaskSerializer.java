56b836d800d9798db9341aa27cc2b8d8468d1332
IsaacOus <iouslimane@myges.fr>
2023-02-19 17:20:20 +0100

    Show tasks in different colors depending on their status

--- src/main/java/org/example/infrastructure/serialization/TaskSerializer.java
+++ src/main/java/org/example/infrastructure/serialization/TaskSerializer.java
@@ -11,16 +11,43 @@ public class TaskSerializer {
 
     public static String serializeToString(TaskWrapper task) {
         StringBuilder serializedTask = new StringBuilder();
-        serializedTask.append(TaskSerializer.repeatChar("\t", task.getNumberOfParentTasks())).append("Task #").append(task.getIndex()).append("\n");
-        serializedTask.append(TaskSerializer.repeatChar("\t", task.getNumberOfParentTasks())).append("[").append(task.getTask().getState().getName()).append("] - \"").append(task.getTask().getDescription()).append("\"\n");
-        serializedTask.append(TaskSerializer.repeatChar("\t", task.getNumberOfParentTasks())).append("#").append(task.getTask().getTag()).append("\n");
-        serializedTask.append(TaskSerializer.repeatChar("\t", task.getNumberOfParentTasks())).append("[Created: ").append(task.getTask().getCreationDate()).append("][Due: ").append(task.getTask().getDueDate()).append("][Closed: ").append(task.getTask().getCloseDate()).append("]\n");
-        serializedTask.append(TaskSerializer.repeatChar("\t", task.getNumberOfParentTasks())).append("SubTasks:\n");
+
+        // set color based on task state
+        String stateColor = "";
+        switch (task.getTask().getState()) {
+            case TODO:
+                stateColor = "\u001B[31m"; // red
+                break;
+            case PROGRESS:
+                stateColor = "\u001B[33m"; // yellow
+                break;
+            case DONE:
+                stateColor = "\u001B[32m"; // green
+                break;
+            case CANCELLED:
+                stateColor = "\u001B[35m"; // magenta
+                break;
+            case CLOSED:
+                stateColor = "\u001B[36m"; // cyan
+                break;
+            case PENDING:
+                stateColor = "\u001B[37m"; // white
+                break;
+            default:
+                break;
+        }
+
+        serializedTask.append(repeatChar("\t", task.getNumberOfParentTasks())).append("Task #").append(task.getIndex()).append("\n");
+        serializedTask.append(repeatChar("\t", task.getNumberOfParentTasks())).append(stateColor).append("[").append(task.getTask().getState().getName()).append("] - \"").append(task.getTask().getDescription()).append("\"").append("\u001B[0m").append("\n");
+        serializedTask.append(repeatChar("\t", task.getNumberOfParentTasks())).append("#").append(task.getTask().getTag()).append("\n");
+        serializedTask.append(repeatChar("\t", task.getNumberOfParentTasks())).append("[Created: ").append(task.getTask().getCreationDate()).append("][Due: ").append(task.getTask().getDueDate()).append("][Closed: ").append(task.getTask().getCloseDate()).append("]\n");
+        serializedTask.append(repeatChar("\t", task.getNumberOfParentTasks())).append("SubTasks:\n");
         if (task.getTask().getSubTasks() == null || task.getTask().getSubTasks().isEmpty()) {
-            serializedTask.append(TaskSerializer.repeatChar("\t", task.getNumberOfParentTasks())).append("\tNone\n");
+            serializedTask.append(repeatChar("\t", task.getNumberOfParentTasks())).append("\tNone\n");
         }
         return serializedTask.toString();
     }
+
 
     private static String repeatChar(String s, int n) {
         return String.valueOf(s).repeat(n);
